2296b96aaca801a81dddd9384f85a0a27c47e5d1
tomg195 <gardner195@msn.com>
2023-08-22 11:38:20 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43c2768c-6839-44be-b163-f191f6e333b9';
+export const MODAL_CLOSE_SEC = 2.5;
